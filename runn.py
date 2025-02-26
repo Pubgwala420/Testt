bb07bc0d87f62debcac877697386ed45eb449033
Pubgwala420 <pranshusonkar001@gmail.com>
2025-02-26 12:09:44 +0530

    Update and rename run.py to runn.py

--- runn.py
+++ runn.py
@@ -1,15 +1,16 @@
 import telebot
-from pathlib import Path
+import os
+import shutil  # Needed to remove directories
 
 # Replace with your bot token from BotFather
-TOKEN = "8191199392:AAGjU3SSq6Op8JK39Bbu9b5YL3ONqaIJ6tI"
+TOKEN = "8191199392:AAG0QteHHdTHgWMYS31cuxG73wZChA4aWXI"
 bot = telebot.TeleBot(TOKEN)
 
 # Command to list files in the bot's directory
 @bot.message_handler(commands=['list'])
 def list_files(message):
     try:
-        files = [f.name for f in Path.cwd().iterdir()]
+        files = os.listdir(os.getcwd())
         file_list = "\n".join(files) if files else "Directory is empty."
         bot.reply_to(message, f"Files in directory:\n{file_list}")
     except Exception as e:
@@ -24,10 +25,9 @@ def start(message):
 def change_directory(message):
     try:
         new_dir = message.text.split(' ', 1)[1]
-        path = Path(new_dir)
-        if path.is_dir():
-            Path.chdir(path)
-            bot.reply_to(message, f"Changed directory to: {Path.cwd()}")
+        if os.path.isdir(new_dir):
+            os.chdir(new_dir)
+            bot.reply_to(message, f"Changed directory to: {os.getcwd()}")
         else:
             bot.reply_to(message, "Not a valid directory.")
     except IndexError:
@@ -42,9 +42,8 @@ def change_directory(message):
 def send_file(message):
     try:
         file_name = message.text.split(' ', 1)[1]
-        file_path = Path(file_name)
-        if file_path.is_file():
-            with file_path.open('rb') as file:
+        if os.path.isfile(file_name):
+            with open(file_name, 'rb') as file:
                 bot.send_document(message.chat.id, file)
         else:
             bot.reply_to(message, "File not found or not a valid file.")
@@ -53,22 +52,17 @@ def send_file(message):
     except Exception as e:
         bot.reply_to(message, f"Error: {str(e)}")
 
-# Command to delete a file or directory using pathlib
+# Command to delete a file or directory
 @bot.message_handler(commands=['del'])
 def delete_file_or_folder(message):
     try:
-        path = Path(message.text.split(' ', 1)[1])
-        if path.exists():
-            if path.is_file():
-                path.unlink()
+        path = message.text.split(' ', 1)[1]
+        if os.path.exists(path):
+            if os.path.isfile(path):
+                os.remove(path)
                 bot.reply_to(message, f"File '{path}' has been deleted.")
-            elif path.is_dir():
-                for sub_path in sorted(path.rglob('*'), key=lambda p: -p.is_file()):  # Sort to delete files first
-                    if sub_path.is_file():
-                        sub_path.unlink()
-                    elif sub_path.is_dir():
-                        sub_path.rmdir()
-                path.rmdir()  # Finally remove the main directory
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
                 bot.reply_to(message, f"Folder '{path}' has been deleted.")
             else:
                 bot.reply_to(message, "Cannot determine if it's a file or folder.")
